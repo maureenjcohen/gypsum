a44b6de041b18038d257d8723402d9300066643a
Maureen Cohen <s1144983@sms.ed.ac.uk>
2021-03-05 22:16:49 +0000

    Added functions for absorption spectrum and transit depth

--- src/config_writer.py
+++ src/config_writer.py
@@ -12,12 +12,24 @@ Model: University of Exeter Stand Alone model of Proxima Centauri b, UM vn11.8
 """
 
 import iris
+import subprocess
 import iris.plot as iplt
 import matplotlib.pyplot as plt
 import matplotlib.cm as mpl_cm
 import numpy as np
+import pandas as pd
+import glob
 
-def profiles(cubes, coords=(-1,45,36)):
+templatepath = 'placeholder'
+
+def write_config(path, cubes, day, coords=(-1,45,36)):
+    
+    """ For one model column:
+        Extracts profiles for: pressure, temperature, water vapour, liquid cloud, ice cloud
+        Calculates/defines profiles for: N2, CO2
+        Reads in a template Planetary Spectrum Generator config file with empty atmosphere layers
+        Writes UM model data atmospheric profiles into atmosphere layers
+        Outputs a text file labelled with the array coordinates of the column for which the profile has been generated"""
     
     for cube in cubes:
         if cube.standard_name == 'air_pressure':
@@ -30,29 +42,75 @@ def profiles(cubes, coords=(-1,45,36)):
             ice_cloud = cube.copy()
         if cube.standard_name == 'mass_fraction_of_cloud_liquid_water_in_air':
             liquid_cloud = cube.copy()
-            
+    # Extract the model data cubes needed
+        
     p0 = iris.coords.AuxCoord(100000.0, long_name='reference_pressure', units='Pa')
     p0.convert_units(air_pressure.units)
     absolute_temp = potential_temp*((air_pressure/p0)**(287.05/1005)) # R and cp in J/kgK for 300K
+    # Convert potential temperature into absolute temperature
     
-    converted_pressure = air_pressure.data*1e-5
-    absolute_temp = absolute_temp.data
-    vapour = (28.0134/18.01528)*spec_humid.data
-    CO2 = np.full(vapour.shape,(28.0134/44.0095)*5.94e-4)
-    N2 = np.ones(vapour.shape) - vapour - CO2
-    liquid_cloud = liquid_cloud.data
-    ice_cloud = ice_cloud.data
+    altitude = air_pressure.coord('level_height').points*1e-3
+    # Extract altitude of T-P points from air pressure cube (in km)
     
-    np.set_printoptions(precision=4, formatter={'float': lambda x: format(x, '.4E')})
+    converted_pressure = air_pressure[day,:,coords[1],coords[2]].data*1e-5 # Convert Pa to bar
+    absolute_temp = absolute_temp[day,:,coords[1],coords[2]].data
+    vapour = (28.0134/18.01528)*spec_humid[day,:,coords[1],coords[2]].data # Convert kg/kg to molecules/molecules
+    # Extract the numpy array for only the column we are looking at (much faster than extracting the full array!)
     
-    print('Air pressure (bar): ' + str(converted_pressure[coords[0],:,coords[1],coords[2]]))
-    print('Air temperature (K): ' +str(absolute_temp[coords[0],:,coords[1],coords[2]]))
-    print('N2 (molecules/molecules): ' + str(N2[coords[0],:,coords[1],coords[2]]))
-    print('Water vapour (molecules/molecules): '+ str(vapour[coords[0],:,coords[1],coords[2]]))
-    print('CO2 (molecules/molecules): ' + str(CO2[coords[0],:,coords[1],coords[2]]))
-    print('Liquid cloud (kg/kg): ' + str(liquid_cloud[coords[0],:,coords[1],coords[2]]))
-    print('Ice cloud (kg/kg): ' + str(ice_cloud[coords[0],:,coords[1],coords[2]]))
+    CO2 = np.full(vapour.shape,(28.0134/44.0095)*5.94e-4) # Model has a fixed amount of CO2. Convert kg/kg to molecules/molecules
+    N2 = np.ones(vapour.shape) - vapour - CO2 # Any gas that isn't vapour or CO2 is N2
+    liquid_cloud = liquid_cloud[day,:,coords[1],coords[2]].data
+    ice_cloud = ice_cloud[day,:,coords[1],coords[2]].data
 
-                                                
+    template = open(str(templatepath) + '/proxb_template.txt','r')    
+    list_in = template.readlines()
+    list_out = list_in.copy()
+    template.close()
+    # Open template PSG config file that already has ProxB planetary data in it, read lines into a list
+    
+    for layer in range(0,39):
+        line_index = 55+layer # Atmosphere layers begin at line 55 of the template
+        list_in[line_index] = list_in[line_index].rstrip() # Strip \n from end of line
+        list_out[line_index] = list_in[line_index] + f"{converted_pressure[layer]:.4E}" + ',' + \
+        f"{absolute_temp[layer]:.4E}" + ',' + f"{altitude[layer]:.4E}" + ',' + f"{N2[layer]:.4E}" + ',' + f"{vapour[layer]:.4E}" + ',' + \
+        f"{CO2[layer]:.4E}" + ',' + f"{liquid_cloud[layer]:.4E}" + ',' + f"{ice_cloud[layer]:.4E}" + '\n'
+        # For each atmosphere layer, add pressure, temperature, N2, H2O, CO2, liquid cloud, and ice cloud data point
+        # Format in scientific notation to 4 decimal places, capital E
+#    print(list_out) # Print to check your list formatting matches examples
+    
+    with open(str(path) + 'configfiles/config_%s_%s.txt' %(coords[1], coords[2]), 'w') as file:
+        for line in list_out:
+            file.write(line)
+            
+    file.close()
+    # Write to a textile file labeled with array column coordinates
+    
+    return(list_out)
+    
 
+def day_generator(path, cubes, day=-1):
+
+    """ Write PSG config files for every column on the limb
+        These can be put through the PSG and the output averaged to get the final spectrum """
+        
+    for latitude in range(0,90):
+        east_config = write_config(path, cubes, day, coords=(-1, latitude, 36))
+        east_filename = str(path) + 'configfiles/config_%s_36.txt' %(latitude)
+        east_cmd = 'curl -d type=all -d whdr=y --data-urlencode file@%s https://psg.gsfc.nasa.gov/api.php' %(east_filename)
+        with open(str(path) + 'spectra/trn_%s_36.txt' %(latitude), 'w') as file:
+            subprocess.run(east_cmd, shell=True, stdout=file)
+        file.close()
+        
+        west_config = write_config(path, cubes, day, coords=(-1, latitude, 108))
+        west_filename = str(path) + 'configfiles/config_%s_108.txt' %(latitude)
+        west_cmd = 'curl -d type=all -d whdr=y --data-urlencode file@%s https://psg.gsfc.nasa.gov/api.php' %(west_filename)
+        with open(str(path) + 'spectra/trn_%s_108.txt' %(latitude), 'w') as file:
+            subprocess.run(west_cmd, shell=True, stdout=file)
+        file.close()
+        print('Up to latitude: ' + str(latitude))
+            
+    
+    
+    
+    
                                                   
